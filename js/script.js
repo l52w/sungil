2653a2c2265def735c72576bc338c7cf19be2188
예인 <kuru040611@gmail.com>
2022-09-22 17:36:43 +0900

    0922

---
+++ js/script.js
@@ -0,0 +1,8 @@
+setInterval(function(){
+    $('#slide>ul').delay(2500);
+    $('#slide>ul').animate({marginLeft:"-1400px"});
+    $('#slide>ul').delay(2500);
+    $('#slide>ul').animate({marginLeft:"-2800px"});
+    $('#slide>ul').delay(2500);
+    $('#slide>ul').animate({marginLeft:"0px"});
+});
