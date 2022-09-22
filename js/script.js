9162228f0589fc393e98020318ba02318a59fa06
예인 <kuru040611@gmail.com>
2022-09-22 18:34:13 +0900

    a

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 setInterval(function(){
-    $('#slide>ul').delay(2500);
+    $('#slide>ul').delay(3500);
     $('#slide>ul').animate({marginLeft:"-1400px"});
-    $('#slide>ul').delay(2500);
+    $('#slide>ul').delay(3500);
     $('#slide>ul').animate({marginLeft:"-2800px"});
-    $('#slide>ul').delay(2500);
+    $('#slide>ul').delay(3500);
     $('#slide>ul').animate({marginLeft:"0px"});
 });
